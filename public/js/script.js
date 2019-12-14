010f5aaf4837afa832cd3ac6bb50921518b17ed4
Sagar <sagarpatel16@gmail.com>
2019-12-14 09:45:26 -0600

    fixed issue

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,16 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $(`.devour-form`).on("click", function(e) {
+    e.preventDefault();
+    console.log("hello");
 
-    $(".devour-form").on("submit", (event) => {
-
-        console.log("hello");
-        e.preventDefault();
-        let burger_id = $(this).children(`.burger_id`).val();
-        console.log(`Burger Id is : ${parseInt(burger_id)}`);
-        $.ajax({
-            type: "PUT",
-            url: `/burgers/${burger_id}`,
-        }).then((res) => {
-            location.reload();
-        })
+    let burger_id = $(this).children(`.burger_id`).val();
+    console.log(typeof burger_id);
+    console.log(`Burger Id is : ${parseInt(burger_id)}`);
+    $.ajax({
+      type: "PUT",
+      url: `/burgers/${burger_id}`
+    }).then(res => {
+      location.reload();
     });
+  });
 });
