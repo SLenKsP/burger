077db0d9ecedd0cd110422e8d348f3bd11d04dfb
Sagar <sagarpatel16@gmail.com>
2019-12-14 10:05:11 -0600

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 let mysql = require(`mysql`);
 
-let connection = mysql.createConnection({
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection= mysql.createConnection({
     host: `localhost`,
     port: 3306,
     user: `root`,
     password: `Burgl4rs`,
     database: `burgers_db`
-});
+  });
+}
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log(`Connected as id: ${ connection.threadId }`);
+connection.connect(err => {
+  if (err) throw err;
+  console.log(`Connected as id: ${connection.threadId}`);
 });
 
 module.exports = connection;
